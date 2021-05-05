8b6534896eb7f3ba7432fd6ffb4d574f81dda8ed
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-05-04 22:16:54 -0400

    Update dockerClient.sh

--- netperf/dockerClient.sh
+++ netperf/dockerClient.sh
@@ -4,8 +4,8 @@ sudo docker build -t netserver .
 
 sudo service netperf stop
 
-netperf -l 60 -H 192.168.1.1 -t TCP_RR -- -r 100,200
-netperf -l 60 -H 192.168.1.1 -t UDP_RR -- -r 100,200
+netperf -l 60 -H 192.168.1.1 -t TCP_RR -- -r 100,200 >> results/docker.log
+netperf -l 60 -H 192.168.1.1 -t UDP_RR -- -r 100,200 >> results/docker.log
 
 wait
 echo Experiment completed
