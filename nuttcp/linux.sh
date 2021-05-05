f984e2e801cb0b08d5f672a115ed1fe6f58f66e2
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-05-04 20:18:03 -0400

    Update linux.sh

--- nuttcp/linux.sh
+++ nuttcp/linux.sh
@@ -4,6 +4,6 @@ now=`date`
 echo "client to server (native), started at $now"
 echo "--------------------------------------------------------------------------------" >> results/linux.log
 echo "client to server (native), started at $now" >> results/linux.log
-sudo perf stat -a nuttcp -t 192.168.1.1 >> linux.log
+sudo perf stat -a -o results/linux.log --append nuttcp -t 192.168.1.1 >> results/linux.log
 echo "" >> results/linux.log
 echo -n "Experiment completed at "; date
