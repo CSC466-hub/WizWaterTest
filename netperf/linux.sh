72bf77cbe83ab8d3eb2e8ae3b76d17fcad0a18c8
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-05-04 20:38:46 -0400

    Update linux.sh

--- netperf/linux.sh
+++ netperf/linux.sh
@@ -4,8 +4,8 @@ now=`date`
 echo "Running netperf, started at $now"
 echo "--------------------------------------------------------------------------------" >> results/linux.log
 echo "Running nerperf TCP_RR, started at $now" >> results/linux.log
-netperf -l 60 -H 10.71.0.24 -t TCP_RR -- -r 100,200 >> results/linux.log
+netperf -l 60 -H 192.168.1.1 -t TCP_RR -- -r 100,200 >> results/linux.log
 echo "Running nerperf TCP_RR, started at $now" >> results/linux.log
-netperf -l 60 -H 10.71.0.24 -t UDP_RR -- -r 100,200 >> results/linux.log
+netperf -l 60 -H 192.168.1.1 -t UDP_RR -- -r 100,200 >> results/linux.log
 echo "" >> results/linux.log
 echo -n "Experiment completed at "; date
