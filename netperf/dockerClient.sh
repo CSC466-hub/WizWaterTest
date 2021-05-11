0e3f3c16112a0457de82bd5f799e57b69ca97fbf
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-05-11 16:59:32 -0400

    Update dockerClient.sh

--- netperf/dockerClient.sh
+++ netperf/dockerClient.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo docker build -t netserver .
-
-sudo service netperf stop
-
+now=`date`
+echo "Running netperf, started at $now"
+echo "--------------------------------------------------------------------------------" >> results/docker.log
+echo "Running nerperf TCP_RR, started at $now" >> results/docker.log
 netperf -l 60 -H 192.168.1.1 -t TCP_RR -- -r 100,200 >> results/docker.log
+echo "Running nerperf UDP_RR, started at $now" >> results/docker.log
 netperf -l 60 -H 192.168.1.1 -t UDP_RR -- -r 100,200 >> results/docker.log
-
-wait
-echo Experiment completed
+echo "" >> results/docker.log
+echo -n "Experiment completed at "; date
